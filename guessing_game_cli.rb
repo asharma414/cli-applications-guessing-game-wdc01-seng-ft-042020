b12634d4978d3867037c9443a9519cb1c7215f4d
Abhinav Sharma <abhi.sharm.414@gmail.com>
2020-04-19 23:58:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,11 +1,11 @@
 def run_guessing_game
   num = rand(1..6)
   input = gets
-  if input = 'exit'
-    return 'Goodbye!'
+  if input == 'exit'
+    puts 'Goodbye!'
   elsif input == num
-    return 'You guessed the correct number!'
+    puts 'You guessed the correct number!'
   else
-    return "Sorry! The computer guessed #{num}"
+    puts "Sorry! The computer guessed #{num}"
   end
 end
