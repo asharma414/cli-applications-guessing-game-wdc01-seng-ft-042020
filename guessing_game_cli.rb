0c092650ab98c9294a69c01379c84dedaea5cb75
Abhinav Sharma <abhi.sharm.414@gmail.com>
2020-04-20 00:06:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,6 +8,6 @@ def run_guessing_game
   elsif input == num
     puts 'You guessed the correct number!'
   elsif input != num
-    puts "Sorry! The computer guessed #{num}"
+    puts "Sorry! The computer guessed #{num}."
   end
 end
